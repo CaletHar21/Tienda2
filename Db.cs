85497eb9e120b2acc8374fc1a734941bc00e6d82
CALETH ABIMAEL TORREZ RAMIREZ <caleth.torrez17@itca.edu.sv>
2024-05-08 23:07:02 -0600

    envio

---
+++ Db.cs
@@ -0,0 +1,55 @@
+﻿namespace TienditaStore.DB
+{
+    public record Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class ProductDB
+    {
+        private static List<Product> _products = new List<Product>()
+       {
+         new Product{ Id=1, Name="Carne", Description="Carne Punta Negra"},
+         new Product{ Id=2, Name="Sopa Maruchan", Description="Sopa Laky"},
+         new Product{ Id=3, Name="Soda", Description="Soda de 3l"}
+       };
+
+        public static List<Product> GetProducts()
+        {
+            return _products;
+        }
+
+        public static Product? GetProduct(int id)
+        {
+            return _products.FirstOrDefault(product => product.Id == id);
+        }
+
+
+        public static Product CreateProduct(Product product)
+        {
+            _products.Add(product);
+            return product;
+        }
+
+        public static Product UpdateProduct(Product update)
+        {
+            _products = _products.Select(product =>
+            {
+                if (product.Id == update.Id)
+                {
+                    product.Name = update.Name;
+                    product.Description = update.Description;
+                }
+                return product;
+            }).ToList();
+            return update;
+        }
+
+        public static void RemoveProduct(int id)
+        {
+            _products = _products.FindAll(product => product.Id != id).ToList();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
+using Microsoft.OpenApi.Models;
+using TienditaStore.DB;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+app.MapPost("/Tienda", (Product product) => ProductDB.CreateProduct(product));
+app.MapGet("/Tienda/{id}", (int id) => ProductDB.GetProduct(id));
+app.MapPut("/Tienda/{id}", (Product product) => ProductDB.UpdateProduct(product));
+app.MapDelete("/Tienda/{id}", (int id) => ProductDB.RemoveProduct(id));
+
 
 app.Run();
